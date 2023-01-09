e124ee97cbde734420ba5d946a60b89d9cd72038
cmunozm <cmunozm@webmain.cl>
2023-01-09 00:30:02 -0300

    Optimizando los checkbox y dejando el array limpio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, VERSION, OnDestroy, Input, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, VERSION, OnDestroy, Input, Output, EventEmitter, ViewChild, AfterViewInit, ElementRef} from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, Subject } from 'rxjs';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
@@ -15,7 +15,7 @@ import { Usuario } from '../app/interface/user.interface';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
 
 	public nameVersion = 'Angular ' + VERSION.full;
   title = 'testDataTable';
@@ -49,7 +49,9 @@ export class AppComponent implements OnInit, OnDestroy {
 	public seleccionadosFilter: Usuario[] = [];
 	public seleccionadosMap: Usuario[] = [];
 
-
+	public allItems: boolean = false;
+	public soloSeleccionados: Usuario[] = [];
+ @ViewChild('checkboxAll', {static: false}) checkboxall!: ElementRef;
 
 
 
@@ -74,7 +76,8 @@ export class AppComponent implements OnInit, OnDestroy {
   return this.AllUserApi.filter( User => User.seleccionado);
 } */
 
-
+	ngAfterViewInit() {
+	}
 	ngOnInit(): void {
 		console.log('%cngOnInit() >>>','background: #d1e7dd; color: #0f5132; padding: 2px 5px;');
 			this.dtOptions = {					
@@ -103,19 +106,16 @@ export class AppComponent implements OnInit, OnDestroy {
 
 		//this.getAllUsers();
   }
-
 	ngOnDestroy(): void {
    console.log('%cngOnDestroy() >>>','background: #d1e7dd; color: #0f5132; padding: 2px 5px;');
 	 this.dtTrigger.unsubscribe();
   }
-
 	editUser(index: number, userId: any) {
 		console.log('%c[DEBUG]: Info usuario para Edit','background: #0d6efd; color: #FFFFFF; padding: 2px 5px;','Info Index >>',index,'Info userID >>', userId)
 	}
 	deleteUser(index: number, userId: any) {
 		console.log('%c[DEBUG]: Info usuario para Delete','background: #6610f2; color: #FFFFFF; padding: 2px 5px;','Info Index >>', index,'Info userID >>', userId);
 	}
-
 	getAllUsers() {
 		this.services.getUsersApi('users').subscribe({
 			next: (response: Usuario[]) => {
@@ -128,143 +128,32 @@ export class AppComponent implements OnInit, OnDestroy {
     	complete: () => console.log('%cHTTP request completed.','background: #d1e7dd; color: #0f5132; padding: 2px 5px;')
 		});
 	}
-
-	//checkAllCheckBox(ev) { // Angular 9
- /*  checkAllCheckBox(ev: any) { // Angular 13
-		this.AllUserApi.forEach( x => x.checked = ev.target.checked);
-		console.log(this.checked);
-	} */
-
-	/* isAllCheckBoxChecked() {
-		return this.AllUserApi.every( p => p.checked);
-		console.log( this.checked);
-	} */
-
-getSelectedItems(row: any, event: any) {
-
-	const inforowForEach = this.AllUserApi.forEach( row => row.selected );
-	console.log('%c.forEach() >>','background: #d1e7dd; color: #0f5132; padding: 2px 5px;', row);
-
-
-
-	const infoRowFilter = this.AllUserApi.filter( item => item.selected);
-	console.log('%c.filter() >>','background: #d1e7dd; color: #0f5132; padding: 2px 5px;', infoRowFilter );
-	 /* if (this.AllUserApi.filter( item => item.selected)) {
-		console.log('%c[DEBUG] filter >>>', 'background:  #cff4fc; color:#084298; padding: 2px 5px;', row);
-  	} */
+	getSelectedItems(row: any, event: any) {
+		/*
+		*@Functions con .filter() sleccionando 1 o mas de 2
+		**/
+		this.seleccionadosFilter = this.AllUserApi.filter( item => item.selected);
+		console.log('%cgetSelectedItems() | .filter() >>','background:  #cff4fc; color:#084298; padding: 2px 5px', this.seleccionadosFilter);
+		this.allItems = false;
+	}
+	selectAll( event: any ) {
+	/*
+	*@Functions con .filter(), seleccionado a todos
+	**/
 	
-	 let infoRowMap = this.AllUserApi.map( (row) => {
-		return row;		
-	 });
-		console.log('%c.map() >>','background: #d1e7dd; color: #0f5132; padding: 2px 5px;',infoRowMap);
-		/* var arr = [2, 5, 6, 3, 8, 9];
-      
-    var newArr = arr.map(function(val, index){
-        return {key:index, value:val*val};
-    }) */
+	this.AllUserApi.filter((selectedAll, index, all: Usuario[]) => {
+		selectedAll.selected = event.target.checked;	
 
-	/* if (this.AllUserApi.map( item => item.selected)) {
-		console.log('%c[DEBUG] .map() >>', 'background:  #cff4fc; color:#084298; padding: 2px 5px;', row);
-  	} */
-	/* const infoRowMap = this.AllUserApi.map(item => {		
-		return item.selected
+		if(this.allItems === true ) {
+			this.soloSeleccionados.push(selectedAll);			
+		} else {
+			this.soloSeleccionados = [];
+			console.log('los 10 son false')
+		}
 	});
-	console.log('%c.map() >>','background: #d1e7dd; color: #0f5132; padding: 2px 5px;', infoRowMap); */
-	/* console.log(marcas); */
-		/* if (this.AllUserApi.map( item => item.selected)) {
-		console.log('%c[DEBUG] .map() >>', 'background:  #cff4fc; color:#084298; padding: 2px 5px;', row);
-  	} */
-
-		
-
-		
+	console.log('%cselectAll | .filter() | this.soloSeleccionados >>','background:#d63384; color: #FFFFFF; padding: 2px 5px;', this.soloSeleccionados);
 	}
-
-	selectAll(event: any) {	
-		//alert('seleccionando toda la data')	
-		this.AllUserApi.forEach((x, index, all: Usuario[]) => { //--> No creo un nuevo arreglo , solo lista la acantidad de objetos dentro del arreglo
-			x.selected = event.target.checked
-			console.log('%c selectAll .forEach() this.seleccionados >>','background: #d1e7dd; color: #0f5132; padding: 2px 5px;', x);
-		
-			/* console.log('%c selectAll forEach (x) >>','background: #d1e7dd; color: #0f5132; padding: 2px 5px;', x);
-			console.log('%c selectAll forEach (index) >>','background: #d1e7dd; color: #0f5132; padding: 2px 5px;', index);
-			console.log('%c selectAll forEach (all) >>','background: #d1e7dd; color: #0f5132; padding: 2px 5px;', all); */
-		});	
-		
-		/* this.seleccionadosMap = this.AllUserApi.map(  (x, index, array: Usuario[]): any => array = event.target.checked );//--> Cre un nuevo arreglo
-		console.log('%c selectAll .map() this.seleccionados >>','background: #d1e7dd; color: #0f5132; padding: 2px 5px;', this.seleccionadosMap); */
-		this.seleccionadosMap = this.AllUserApi.map(function(x, index, array: Usuario[]): any {
-				 return array = event.target.checked
-		} );//--> Cre un nuevo arreglo
-		console.log('%c selectAll .map() this.seleccionados >>','background: #d1e7dd; color: #0f5132; padding: 2px 5px;', this.seleccionadosMap);
-
-		this.seleccionados = this.AllUserApi.map( ( x, index, Array:any)  => {
-			//console.log('si hay data...', Array);
-			   if(x.selected = event.target.checked) {
-					console.log('si hay data...', x);
-										
-			} else {
-				console.log('no hay data...');
-			}   
-			 return x
-		});
-		 console.log('%c const seleccionados >>', this.seleccionados);
-
-		
-		this.seleccionadosFilter = this.AllUserApi.filter((data, index, all: Usuario[]) => {
-			return all;
-		})
-		console.log('%c selectAll .filter() this.seleccionadosFilter >>','background: #d1e7dd; color: #0f5132; padding: 2px 5px;', this.seleccionadosFilter);
-		
-		/* 
-		
-		 if (this.isChecked == true) {
-       $('.checkboxCls').prop('checked', false);
-      this.isChecked = false;
-    } else {
-       $('.checkboxCls').prop('checked', true);
-      this.isChecked = true;
-    }
-		
-		*/
-		 
-  	//this.AllUserApi.forEach( x => x.selected = event.target.checked);
-		/* this.AllUserApi.forEach( x => x.selected = event.target.checked); */	
-		/* this.AllUserApi.forEach( (all,index, todos) => {
-			 all.selected = event.target.checked
-			 console.log(all)
-		});	 */
-		/* this.AllUserApi.map( (usuarios) => {
-			 this.seleccionados = usuarios;
-		});
-	 */
-
-
-		
-	}
-
-	/*  selectAll(event: any) {
-			
-  const infoAllRow = this.AllUserApi.forEach( item => item.selected = event.target.checked);
-	 this.selectionChanged.emit(this.getSelectedItems());  
-	console.log('%c All rows .forEach() >>','background: #d1e7dd; color: #0f5132; padding: 2px 5px;', infoAllRow);
-	} */ 
-	selectedDataAll() {		
-		/* const all = this.AllUserApi.forEach( item => this.selectAll);
-		console.log('que devuelvo', all); */
-	 /* this.AllUserApi.forEach( function(item, index, allUser) {
-			console.log('item >>', item);
-			console.warn('index >>', index); 
-			console.info('allUser >>', allUser); 
-		})   */
-		/* console.log('%c All rows .forEach() >>','background: #d1e7dd; color: #0f5132; padding: 2px 5px;', event); */
-	} 
-	selectAllChanged() {
-  	//this.AllUserApi.forEach( item => item.selected = this.selectAll);
-	}
-
 	
-
 }
 
 
